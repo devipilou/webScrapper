750a181e1a69eb92daf8a18ce4ec7117ca50f2f4
Pierre-Loup Perez <devipilou@gmail.com>
2021-12-08 00:34:22 +0100

    add client-side

--- index.js
+++ index.js
@@ -2,67 +2,102 @@ const express = require('express')
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 
+const path = require('path');
+
 const PORT = 8000; //Port we wanna use
 
 // creation of an anonymous async function
 (async () => {
-    // dedicated
+    // DEDICATED
     const dedicatedUrl = 'https://www.dedicatedbrand.com/en/women/sweats#page=2';
     // this object will contain:
     // an array of products
     // average price
     // the amount of products
-    const dedicatedData = await scrapeData(dedicatedUrl, '.productList-container .productList','.productList-details', '.productList-title','.productList-price');
-    console.log(dedicatedData);
+    const dedicatedData = await scrapeData(dedicatedUrl, '.stickyLogo-text','.productList-container .productList', '.productList-details', '.productList-title', '.productList-price');
+    // console.log(dedicatedData);
 
-    // organic basics
+    // ORGANIC BASICS
     const organicBasicsUrl = 'https://organicbasics.com/collections/all-womens-products?filter=sweaters';
-    const organicBasicsData = await scrapeData(organicBasicsUrl, '.product-grid-item-container[data-tags*=Sweater]', '.product__grid--text-container', 'a', '.product-price')
-    console.log(organicBasicsData)
+    const organicBasicsData = await scrapeData(organicBasicsUrl, '#Logo','.product-grid-item-container[data-tags*=Sweater]', '.product__grid--text-container', 'a', '.product-price')
+    // console.log(organicBasicsData)
 })();
 
 
 
-function scrapeData(url, itemProductCssSelector, itemProductInfosCssSelector, itemProductTitleCssSelector, itemProductPriceCssSelector, itemProductImgCssSelector = 'img'){
+function scrapeData(url, logoCssSelector, itemProductCssSelector, itemProductInfosCssSelector, itemProductTitleCssSelector, itemProductPriceCssSelector, itemProductImgCssSelector = 'img') {
     const obj = {};
     return axios(url)
-    .then(response => {
-        const html = response.data;
+        .then(response => {
+            const html = response.data;
 
-        const $ = cheerio.load(html);
+            const $ = cheerio.load(html);
 
-        // Sum of all the prices for average price
-        let sumOfPrices = 0;
+            // Sum of all the prices for average price
+            let sumOfPrices = 0;
 
-        obj.products = [];
-        
+            // get logo
+            const logoTag = $(logoCssSelector, html);
+            if(logoTag[0].name === 'img'){
+                obj.logo = logoTag.attr('src');
+            }else if (logoTag[0].name === 'svg'){
+                obj.logo = logoTag.get().map(svg => $.html(svg))[0];
+            }
 
-        $(itemProductCssSelector, html).each(function() {
-            const infos = $(this).find(itemProductInfosCssSelector);
+            obj.products = [];
+            
 
-            const title = infos.find(itemProductTitleCssSelector).text();
-            const price = infos.find(itemProductPriceCssSelector).text().trim();
-            const img = $(this).find(itemProductImgCssSelector).attr('src');
 
-            // add to the array
-            obj.products.push({
-                title,
-                price,
-                img
-            })
+            $(itemProductCssSelector, html).each(function () {
+                const infos = $(this).find(itemProductInfosCssSelector);
 
-            // sum of the prices
-            sumOfPrices += price.includes('€') ? parseInt(price.replace('€', '')) : parseInt(price.replace('EUR', ''));
-        });
+                const title = infos.find(itemProductTitleCssSelector).text();
+                const price = infos.find(itemProductPriceCssSelector).text().trim();
+                const img = $(this).find(itemProductImgCssSelector).attr('src');
 
-        const average = sumOfPrices / obj.products.length;
-        obj.average = average;
-        obj.count = obj.products.length
+                // add to the array
+                obj.products.push({
+                    title,
+                    price,
+                    img
+                })
 
-        return obj;
-    })
+                // sum of the prices
+                sumOfPrices += price.includes('€') ? parseInt(price.replace('€', '')) : parseInt(price.replace('EUR', ''));
+            });
+
+            const average = sumOfPrices / obj.products.length;
+            obj.average = average;
+            obj.count = obj.products.length
+            obj.url = url;
+
+            return obj;
+        })
 }
 
 // init Express
 const app = express();
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+
+app.get('/api/', async (req, res) => {
+    // DEDICATED
+    const dedicatedUrl = 'https://www.dedicatedbrand.com/en/women/sweats#page=2';
+    // this object will contain:
+    // an array of products
+    // average price
+    // the amount of products
+    const dedicatedData = await scrapeData(dedicatedUrl, '.productList-container .productList', '.productList-details', '.productList-title', '.productList-price');
+
+    // ORGANIC BASICS
+    const organicBasicsUrl = 'https://organicbasics.com/collections/all-womens-products?filter=sweaters';
+    const organicBasicsData = await scrapeData(organicBasicsUrl, '.product-grid-item-container[data-tags*=Sweater]', '.product__grid--text-container', 'a', '.product-price')
+    return res.send({
+        dedicatedData,
+        organicBasicsData
+    });
+})
+
+// Creating the client-side page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
